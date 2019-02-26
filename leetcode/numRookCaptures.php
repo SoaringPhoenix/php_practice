855bd6e27503e49062f76c8cfd50d8d76f6cea93
Jason <jason_0313@outlook.com>
2019-02-26 23:36:17 +0800

    写了两笔

--- leetcode/numRookCaptures.php
+++ leetcode/numRookCaptures.php
@@ -10,36 +10,49 @@ class Solution {
         $rowNum = 0;
         $colNum = 0;
         $count = 0;
-        $arr = [];
         foreach ($board as $index => $row) {
-            if (in_array('R', $row)) {
-                $rowNum = $index;
-                $colNum = array_search('R', $board[$index]);
-                for ($i = $colNum; $i >= 0; $i--) {
-                    if ($board[$index][$i] == 'p') {
-                        $count += 1;
-                        break;
-                    }
-                    if ($board[$index][$i] == 'B') {
-                        break;
-                    }
-                }
-                for ($i = $colNum; $i <= 7; $i++) {
-                    if ($board[$index][$i] == 'p') {
-                        $count += 1;
-                        break;
-                    }
-                    if ($board[$index][$i] == 'B') {
-                        break;
-                    }
+            foreach ($board[$index] as $idx => $col) {
+                if ($board[$index][$idx] == 'R') {
+                    $rowNum = $index;
+                    $colNum = $idx;
                 }
             }
         }
-        foreach ($board as $index => $row) {
-
+        for ($i = $colNum; $i >= 0; $i--) {
+            if ($board[$rowNum][$i] == 'p') {
+                $count += 1;
+                break;
+            }
+            if ($board[$rowNum][$i] == 'B') {
+                break;
+            }
+        }
+        for ($i = $colNum; $i <= 7; $i++) {
+            if ($board[$rowNum][$i] == 'p') {
+                $count += 1;
+                break;
+            }
+            if ($board[$rowNum][$i] == 'B') {
+                break;
+            }
+        }
+        for ($j = $rowNum; $j >= 0; $j++) {
+            if ($board[$j][$colNum] == 'p') {
+                $count += 1;
+                break;
+            }
+        }
+        for ($j = $rowNum; $j <= 7; $j++) {
+            if ($board[$j][$colNum] == 'B') {
+                break;
+            }
         }
         return $count;
     }
 }
 $board = [[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".","R",".",".",".","p"],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]];
 var_export((new Solution())->numRookCaptures($board));
+
+/*
+ * 3000个组合，
+ */
