3a1de845b3fdae4e781d6f0f77b9122a4c7e3b49
= <=>
2019-03-01 20:55:17 +0800

    感觉阅读世界要测不完了

--- leetcode/findWords.php
+++ leetcode/findWords.php
@@ -5,12 +5,8 @@
  * Date: 2019/2/28
  * Time: 19:13
  */
-<<<<<<< HEAD
+
 class Solution {
-=======
-class Solution
-{
->>>>>>> b86728773e1fc0563293a321adebb6f2d5e14ac0
     function findWords($words) {
         $alphabet = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
         foreach ($words as $index => $word) {
@@ -19,11 +15,7 @@ class Solution
             stripos($alphabet[2], $words[$index][0]) !== false && $spot = 2;
             foreach (str_split($words[$index]) as $idx => $letter) {
                 if ($idx > 0) {
-<<<<<<< HEAD
                     if (stripos($alphabet[$spot], $letter) === false) {
-=======
-                    if (stripos($alphabet[$spot], $letter) == false) {
->>>>>>> b86728773e1fc0563293a321adebb6f2d5e14ac0
                         unset($words[$index]);
                         break;
                     }
