8b2e7e096d9b13f6495c9a0bd7e5e855aeec380b
= <=>
2019-03-14 20:04:12 +0800

    fairCandy

---
+++ leetcode/fairCandySwap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zuoyebang
+ * Date: 2019/3/14
+ * Time: 19:56
+ */
+class Solution {
+    function fairCandySwap($A, $B) {
+        $diff = (array_sum($B) - array_sum($A)) / 2;
+    }
+}
