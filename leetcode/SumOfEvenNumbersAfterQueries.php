8f859440a57d03211fd32de3bc497b2c68eec9fb
= <=>
2019-03-14 19:46:53 +0800

    代码性能太太差！

--- leetcode/SumOfEvenNumbersAfterQueries.php
+++ leetcode/SumOfEvenNumbersAfterQueries.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: jason
@@ -7,16 +8,36 @@
  */
 class Solution {
     function sumEvenAfterQueries($A, $queries) {
+        $odd = [];
+        $origin = $A;
+        foreach ($A as $elem) {
+            if ($elem & 1) {
+                array_push($odd, $elem);
+                $A = array_diff($A, [$elem]);
+            }
+        }
         foreach ($queries as $index => $query) {
-            $A[$queries[$index][1]] += $query[0];
-            $queries[$index] = 0;
-            foreach ($A as $idx => $elem) {
-                $elem % 2 == 0 && $queries[$index] += $elem;
+            if ($query[0] & 1) {
+                if ($origin[$query[1]] & 1) {
+                    $queries[$index] = array_sum($A) + $query[0] + $origin[$query[1]];
+                    array_push($A, $query[0] + $origin[$query[1]]);
+                } else {
+                    $queries[$index] = array_sum($A) - $origin[$query[1]];
+                    $A[$index] = $A[$index] + $query[0];
+                    $A = array_diff($A, [$A[$index]]);
+                }
+            } else {
+                if (count($A) == 0) {$queries = [0];continue;}
+                $A[$index] += $query[0];
+                $queries[$index] = array_sum($A);
             }
         }
         return $queries;
     }
 }
+
 $A = [1, 2, 3, 4];
+$A = [3, 2];
 $queries = [[1, 0], [-3, 1], [-4, 0], [2, 3]];
+$queries = [[4, 0], [3, 0]];
 var_export((new Solution())->sumEvenAfterQueries($A, $queries));
